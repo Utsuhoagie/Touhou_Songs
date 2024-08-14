acb8aed33248b26581cade30cc5f14438c7b9a38
Utsuhoagie <thiendangssb@gmail.com>
2024-08-14 22:07:15 +0700

    [infra] add FileResponse

--- Server/Infrastructure/API/ApiController.cs
+++ Server/Infrastructure/API/ApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Touhou_Songs.App.DataManagement.Features;
 using Touhou_Songs.Infrastructure.Results;
 
 namespace Touhou_Songs.Infrastructure.API;
@@ -8,4 +9,8 @@ namespace Touhou_Songs.Infrastructure.API;
 public abstract class ApiController : ControllerBase
 {
 	public IActionResult ToResponse<T>(Result<T> res) => StatusCode((int)res.StatusCode, res);
+
+	public IActionResult ToResponse(Result<FileResponse> res) => res.Success
+		? File(res.Value!.Contents, res.Value.MimeType, res.Value.FileName)
+		: ToResponse(res);
 }
---
+++ Server/Infrastructure/Results/FileResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Touhou_Songs.Infrastructure.Results;
+
+public record FileResponse
+{
+	public byte[] Contents { get; set; } = [];
+	public string MimeType { get; set; } = string.Empty;
+	public string FileName { get; set; } = string.Empty;
+}
